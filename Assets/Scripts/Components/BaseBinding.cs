e87396972caa82830409f50cb562b76e9e9e5fa3
Aditya Yuwono <aditya@labster.com>
2014-09-02 10:19:29 +0800

    Added new binding class to manage scaling of crosshair

---
+++ Assets/Scripts/Components/BaseBinding.cs
@@ -0,0 +1,35 @@
+﻿using Scripts.Core;
+using Scripts.Helpers;
+
+namespace Scripts.Components
+{
+    public class BaseBinding : BaseController
+    {
+        private Property _property;
+        public void SetProperty(Property property)
+        {
+            _property = property;
+        }
+
+        private void Start()
+        {
+            Bind();
+        }
+
+        private void OnDestroy()
+        {
+            Unbind();
+        }
+
+        protected virtual void Bind()
+        {
+            _property.OnChange += OnChanged;
+        }
+
+        protected virtual void Unbind()
+        {
+            _property.OnChange -= OnChanged;
+        }
+        protected virtual void OnChanged() { }
+    }
+}
