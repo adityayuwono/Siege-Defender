adf7a7f18d231a922801e644471787c84ff84e36
Aditya Yuwono <aditya@labster.com>
2014-09-02 10:18:55 +0800

    Separated constant values to their own static var

--- Assets/Scripts/Components/FollowMouse.cs
+++ Assets/Scripts/Components/FollowMouse.cs
@@ -6,6 +6,9 @@ namespace Scripts.Components
 {
     public class FollowMouse : MouseInteraction
     {
+        private const float TOUCH_DEVIATION = 1.2f;
+        private const string CROSSHAIR_ASSET_PATH = "GUIs/Crosshair";
+
         private Camera _mainCamera;
 
         private void Start()
@@ -14,21 +17,18 @@ namespace Scripts.Components
         }
 
         private Texture2D _crosshairImage;
-
         private TargetViewModel _viewModel;
-
         private static Vector2 _halfScreen;
 
         protected override void OnSetup()
         {
             base.OnSetup();
-
-            _halfScreen = new Vector2(Screen.width/2f, Screen.height/2f);
-
             _viewModel = ViewModel as TargetViewModel;
+            
+            _halfScreen = new Vector2(Screen.width / 2f, Screen.height / 2f);
 
-            _crosshairImage = Resources.Load<Texture2D>("GUIs/Crosshair");
-
+            _crosshairImage = Resources.Load<Texture2D>(CROSSHAIR_ASSET_PATH);
+            
             _circleRect = new Rect(
                 (Screen.width * _viewModel.Index) - (_viewModel.Index * Screen.height * Values.GUI_CIRCLE_SIZE_F),
                 Screen.height * (1 - Values.GUI_CIRCLE_SIZE_F),
@@ -55,7 +55,7 @@ namespace Scripts.Components
             inputPosition.y = Screen.height - inputPosition.y;
             if (_circleRect.Contains(inputPosition))
             {
-                var relativeToCenter = (inputPosition - _circleCenter)*1.2f;
+                var relativeToCenter = (inputPosition - _circleCenter) * TOUCH_DEVIATION;
 
                 _crosshairRect.x = _halfScreen.x + (_halfScreen.x * (relativeToCenter.x / (_circleRect.width / 2f)));
                 _crosshairRect.y = _halfScreen.y + (_halfScreen.y * (relativeToCenter.y / (_circleRect.height/ 2f)));
