fca4ac73198672aece61b354fa9560e7fd5eb77f
Aditya Yuwono <aditya@labster.com>
2014-09-07 00:24:35 +0800

    Added system to register all ViewModels into 1 dictionary, heavy stuff

--- Assets/Scripts/EngineBase.cs
+++ Assets/Scripts/EngineBase.cs
@@ -60,6 +60,60 @@ namespace Scripts
             throw new EngineException(this, string.Format("ObjectModel not found, Id: {0}", id));
         }
 
+        #region ViewModel Lookup
+        // I didn't expect i had to go this far, seriously
+        private readonly Dictionary<string, BaseViewModel> _viewModels = new Dictionary<string, BaseViewModel>();
+        public T GetViewModel<T>(string id) where T : BaseViewModel
+        {
+            if (!_viewModels.ContainsKey(id))
+                throw new EngineException(this, string.Format("Failed to find ViewModel with Id: {0}", id));
+
+            var vm = _viewModels[id] as T;
+            return vm;
+        }
+        public void RegisterViewModel(BaseViewModel viewModel)
+        {
+            if (_viewModels.ContainsKey(viewModel.Id))
+                throw new EngineException(this, string.Format("Duplicate Id: {0}", viewModel.Id));
+
+            _viewModels.Add(viewModel.Id, viewModel);
+        }
+        #endregion
+
+        #region Property Lookup
+        private readonly Dictionary<string, Dictionary<string, Property>> _properties = new Dictionary<string, Dictionary<string, Property>>(); 
+        public void RegisterProperty(BaseViewModel viewModel, string id, Property property)
+        {
+            if (_properties.ContainsKey(id))
+            {
+                // We already register that type of property, let's add to the list
+                var propertyDict = _properties[id];
+                if (propertyDict.ContainsKey(viewModel.Id))
+                    // Woops, duplicate
+                    throw new EngineException(this, string.Format("Failed to register property: {0} for ViewModel: {1}, Duplicate is found", id, viewModel.Id));
+
+                // OK, everything is good, add a new property
+                propertyDict.Add(viewModel.Id, property);
+            }
+            else
+            {
+                // No similar property registered yet, meaning this is the first of it's kind, momentous
+                _properties.Add(id, new Dictionary<string, Property> {{viewModel.Id, property}});
+            }
+        }
+
+        public Property GetProperty(string viewModelId, string propertyId)
+        {
+            if (!_properties.ContainsKey(propertyId))
+                throw new EngineException(this, string.Format("Failed to find Property with Id: {0}", propertyId));
+
+            var propertyDict = _properties[propertyId];
+            if (!propertyDict.ContainsKey(viewModelId))
+                throw new EngineException(this, string.Format("Failed to find Property with Id: {0} of ViewModel: {1}", propertyId, viewModelId));
+
+            return propertyDict[viewModelId];
+        }
+        #endregion
         public virtual EnemyBaseModel GetEnemy(string enemyId)
         {
             throw new System.NotImplementedException();
