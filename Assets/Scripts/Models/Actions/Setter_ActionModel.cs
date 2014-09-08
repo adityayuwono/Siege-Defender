e52482929b281bb2b4f940eb9ee243c76724ac43
Aditya Yuwono <aditya@labster.com>
2014-09-08 19:42:45 +0800

    Added new action to allow defining property change of another view model

---
+++ Assets/Scripts/Models/Actions/Setter_ActionModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace Scripts.Models.Actions
+{
+    public class Setter_ActionModel : Base_ActionModel
+    {
+        [XmlAttribute]
+        public string Property { get; set; }
+
+        [XmlAttribute]
+        public string Value { get; set; }
+    }
+}
---
+++ Assets/Scripts/ViewModels/Actions/Setter_ActionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Scripts.Core;
+using Scripts.Models.Actions;
+
+namespace Scripts.ViewModels.Actions
+{
+    public class Setter_ActionViewModel : Base_ActionViewModel
+    {
+        private readonly Setter_ActionModel _model;
+        public Setter_ActionViewModel(Setter_ActionModel model, BaseViewModel parent) : base(model, parent)
+        {
+            _model = model;
+        }
+
+        public override void Invoke()
+        {
+            var property = Root.GetProperty(_model.Target, _model.Property);
+
+            if (property is Property<string>)
+            {
+                var stringProperty = property as AdjustableProperty<string>;
+                stringProperty.SetValue(_model.Value);
+            }
+            else if (property is Property<bool>)
+            {
+                var boolProperty = property as AdjustableProperty<bool>;
+                boolProperty.SetValue(bool.Parse(_model.Value));
+            }
+        }
+    }
+}
