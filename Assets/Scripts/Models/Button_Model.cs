be979b9f9f1dc2ddf9cb69d7aea85c6b430def8c
Aditya Yuwono <aditya@labster.com>
2014-09-10 21:32:16 +0800

    Added new action to change another vm's property

--- Assets/Scripts/Models/Button_Model.cs
+++ Assets/Scripts/Models/Button_Model.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using Scripts.Models.Actions;
+using Scripts.Models.GUIs;
 
 namespace Scripts.Models
 {
     [Serializable]
-    public class Button_Model : ElementModel
+    public class Button_Model : BaseGUIModel
     {
         [XmlElement(ElementName = "LoadScene", Type = typeof(LoadScene_ActionModel))]
-        public List<Action_Model> Actions { get; set; }
+        [XmlElement(ElementName = "Setter", Type = typeof(Setter_ActionModel))]
+        public List<Base_ActionModel> Actions { get; set; }
     }
 }
