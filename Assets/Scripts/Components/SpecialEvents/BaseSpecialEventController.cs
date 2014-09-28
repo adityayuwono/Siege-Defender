9feb87ac6595c3ef7adcd028541e3101b45366eb
Aditya Yuwono <aditya@labster.com>
2014-09-28 20:45:07 +0800

    Added base class for special events

---
+++ Assets/Scripts/Components/SpecialEvents/BaseSpecialEventController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Scripts.Components.SpecialEvents
+{
+    public class BaseSpecialEventController : MonoBehaviour
+    {
+        public void StartSpecialEvent(EngineBase engine)
+        {
+            engine.StartCoroutine(StartSpecialEvent());
+        }
+
+        protected virtual IEnumerator StartSpecialEvent()
+        {
+            yield return null;
+        }
+    }
+}
