6e227ebf70c38831ba7bfd4a1d82361d9b56c185
Aditya Yuwono <aditya@labster.com>
2014-09-29 18:08:59 +0800

    Added Air Balloon event controller

--- Assets/Scripts/Components/SpecialEvents/BaseSpecialEventController.cs
+++ Assets/Scripts/Components/SpecialEvents/BaseSpecialEventController.cs
@@ -1,16 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Scripts.Components.SpecialEvents
 {
     public class BaseSpecialEventController : MonoBehaviour
     {
+        public Action OnSpecialEventFinished;
+
         public void StartSpecialEvent(EngineBase engine)
         {
-            engine.StartCoroutine(StartSpecialEvent());
+            engine.StartCoroutine(EnumerateSpecialEvent());
         }
 
-        protected virtual IEnumerator StartSpecialEvent()
+        protected virtual IEnumerator EnumerateSpecialEvent()
         {
             yield return null;
         }
