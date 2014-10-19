f6845360a3f64543e6c4fd6426af62851dfc5cfe
Aditya Yuwono <aditya@labster.com>
2014-10-20 00:40:19 +0800

    Fixed calculation of the x angle in Accelerometer camera controller

--- Assets/Scripts/Components/AccelerometerController.cs
+++ Assets/Scripts/Components/AccelerometerController.cs
@@ -8,19 +8,25 @@ namespace Scripts.Components
     public class AccelerometerController : BaseController
     {
         // We need to keep track of the rotation on our own in Unity3d
+        private float _xRotationAngle;
         private float _yRotationAngle;
 
         private void Update()
         {
             var xClamped = -Input.acceleration.z/2f;
-            var xAngle = 27.5f + (xClamped*30f);
+            var xAngle = /*27.5f+*/ (xClamped*30f);
 
             // Between 90 to the left, and 90 to the right
             var yClamped = Input.acceleration.x*90f;
             _yRotationAngle += (yClamped - _yRotationAngle)*Time.deltaTime;
-            
-            transform.eulerAngles += (new Vector3(xAngle, 0, 0) - transform.eulerAngles) * Time.deltaTime;
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, _yRotationAngle, transform.eulerAngles.z);
+
+            _xRotationAngle += (xAngle - _xRotationAngle)*Time.deltaTime;
+            transform.eulerAngles = new Vector3(_xRotationAngle, _yRotationAngle, transform.eulerAngles.z);
+        }
+
+        private void OnGUI()
+        {
+            GUI.Label(new Rect(0,35,100,100), Input.acceleration+"\n"+transform.eulerAngles.ToString());
         }
     }
 }
