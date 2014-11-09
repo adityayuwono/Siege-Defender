76f36feac735711db4b70c871f88e46c2b1e8927
Aditya Yuwono <aditya@labster.com>
2014-11-09 16:06:37 +0800

    Changed Load Scene logic to use the Value property from Load Scene Model

--- Assets/Scripts/ViewModels/Actions/LoadSceneAction.cs
+++ Assets/Scripts/ViewModels/Actions/LoadSceneAction.cs
@@ -15,7 +15,7 @@ namespace Scripts.ViewModels
         {
             base.Invoke();
 
-            Root.ChangeScene(_model.Target, _model.LevelId);
+            Root.ChangeScene(_model.Value, _model.LevelId);
         }
     }
 }
