69471f9bb63444c1b0256626d9a6bb584758998c
Aditya Yuwono <aditya@labster.com>
2014-11-09 16:06:37 +0800

    Changed Load Scene logic to use the Value property from Load Scene Model

--- Assets/Scripts/ViewModels/Actions/LoadSceneAction.cs
+++ Assets/Scripts/ViewModels/Actions/LoadSceneAction.cs
@@ -1,4 +1,5 @@
-﻿using Scripts.Models;
+﻿using Scripts.Helpers;
+using Scripts.Models;
 using Scripts.ViewModels.Actions;
 
 namespace Scripts.ViewModels
@@ -9,13 +10,16 @@ namespace Scripts.ViewModels
         public LoadSceneAction(LoadSceneActionModel model, Base parent) : base(model, parent)
         {
             _model = model;
+
+            if (!string.IsNullOrEmpty(_model.Target))
+                throw new EngineException(this, "Doesn't need Target, specify the scene id in the Value");
         }
 
         public override void Invoke()
         {
             base.Invoke();
 
-            Root.ChangeScene(_model.Target, _model.LevelId);
+            Root.ChangeScene(_model.Value, _model.LevelId);
         }
     }
 }
