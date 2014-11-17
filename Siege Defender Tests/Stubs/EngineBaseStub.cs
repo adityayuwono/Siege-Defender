7902f5287b99ff9aeec70c5d430aa208a58ffa3c
Aditya Yuwono <aditya@labster.com>
2014-11-17 09:47:48 +0800

    Expanded engine stub to allow more testing

--- Siege Defender Tests/Stubs/EngineBaseStub.cs
+++ Siege Defender Tests/Stubs/EngineBaseStub.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Scripts;
-using Scripts.Components;
+using Scripts.Interfaces;
 using Scripts.Models;
+using Scripts.ViewModels.Enemies;
 
 namespace SiegeDefenderTests.Stubs
 {
@@ -11,9 +13,55 @@ namespace SiegeDefenderTests.Stubs
         {
         }
 
-        public override IntervalRunner IntervalRunner
+        public override PlayerSettingsModel PlayerSettingsModel
         {
-            get { throw new NotImplementedException(); }
+            get { return _playerSettingsModel; }
+        }
+        private PlayerSettingsModel _playerSettingsModel;
+        public void SetPlayerSettingsModel(PlayerSettingsModel playerSettingsModel)
+        {
+            _playerSettingsModel = playerSettingsModel;
+        }
+
+        private readonly IntervalRunnerStub _intervalRunner = new IntervalRunnerStub();
+        public override IIntervalRunner IntervalRunner
+        {
+            get { return _intervalRunner; }
+        }
+
+        public void AddLootTable(LootTable lootTable)
+        {
+            _lootTables.Add(lootTable.Id, lootTable);
+        }
+
+        public void AddItemModel(ItemModel itemModel)
+        {
+            _items.Add(itemModel.Id, itemModel);
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("Saving...");
+        }
+    }
+
+    public class IntervalRunnerStub : IIntervalRunner
+    {
+        private readonly List<Action> _subscribedActions = new List<Action>();
+
+        public void SubscribeToInterval(Action action, float delay = 0, bool startImmediately = true)
+        {
+            _subscribedActions.Add(action);
+        }
+
+        public bool UnsubscribeFromInterval(Action action)
+        {
+            if (!_subscribedActions.Contains(action))
+                return false;
+
+            _subscribedActions.Remove(action);
+            
+            return true;
         }
     }
 }
