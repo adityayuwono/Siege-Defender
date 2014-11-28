0de19ee4a75f2b561d07a33f3782dde53417c048
Aditya Yuwono <aditya@labster.com>
2014-11-28 10:04:08 +0800

    Removed checking for target in Load Scene Action

--- Assets/Scripts/ViewModels/Actions/LoadSceneAction.cs
+++ Assets/Scripts/ViewModels/Actions/LoadSceneAction.cs
@@ -1,5 +1,4 @@
-﻿using Scripts.Helpers;
-using Scripts.Models;
+﻿using Scripts.Models;
 using Scripts.ViewModels.Actions;
 
 namespace Scripts.ViewModels
@@ -10,16 +9,18 @@ namespace Scripts.ViewModels
         public LoadSceneAction(LoadSceneActionModel model, Base parent) : base(model, parent)
         {
             _model = model;
-
-            if (!string.IsNullOrEmpty(_model.Target))
-                throw new EngineException(this, "Doesn't need Target, specify the scene id in the Value");
         }
 
         public override void Invoke()
         {
             base.Invoke();
 
-            Root.ChangeScene(_model.Value, _model.LevelId);
+            Root.ChangeScene(_model.Target, _model.LevelId);
+        }
+
+        protected override object FindTarget()
+        {
+            return null;
         }
     }
 }
