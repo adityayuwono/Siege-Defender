b021362b526db661f98d30723412eb93bfaa7a4d
Aditya Yuwono <aditya@labster.com>
2018-06-11 21:33:17 +0700

    Reset default value of enchantments

--- Assets/Scripts/Models/ProjectileEnchantments.cs
+++ Assets/Scripts/Models/ProjectileEnchantments.cs
@@ -6,5 +6,20 @@ namespace Scripts.Models
 	[Serializable]
 	public class ProjectileEnchantments : ProjectileModel
 	{
+		public ProjectileEnchantments()
+		{
+			DeathDelay = 0f;
+
+			DamageSerialized = "0-0";
+			RoF = 0f;
+			Ammunition = 0;
+			ReloadTime = 0f;
+			Accuracy = 0f;
+			CriticalChance = 0f;
+			CriticalDamageMultiplier = 0f;
+			Deviation = 0f;
+			SpeedDeviationSerialized = "0-0";
+			Scatters = 0;
+		}
 	}
 }
