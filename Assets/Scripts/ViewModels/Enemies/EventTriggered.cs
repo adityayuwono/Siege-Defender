ddef8de3b1c2fa01131be3229230e97ebaca3866
Aditya Yuwono <aditya@labster.com>
2018-06-27 12:32:02 +0700

    Separate event hooks into methods

--- Assets/Scripts/ViewModels/Enemies/EventTriggered.cs
+++ Assets/Scripts/ViewModels/Enemies/EventTriggered.cs
@@ -25,6 +25,22 @@ namespace Scripts.ViewModels.Enemies
 
 		protected override void OnActivate()
 		{
+			HookEvents();
+		}
+		
+		protected override void OnDeactivate()
+		{
+			UnhookEvents();
+
+			if (_isInvoked)
+			{
+				_isInvoked = false;
+				base.OnDeactivate();
+			}
+		}
+
+		private void HookEvents()
+		{
 			_parentObject = GetParent<Object>();
 			if (_parentObject == null)
 			{
@@ -34,99 +50,205 @@ namespace Scripts.ViewModels.Enemies
 			switch (_model.Event)
 			{
 				case Event.Interrupt:
-				{
-					var boss = _parentObject as Boss;
-					if (boss != null)
 					{
-						boss.OnInterrupt += InvokeEvent;
+						var boss = _parentObject as Boss;
+						if (boss != null)
+						{
+							boss.OnInterrupt += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.Click:
-				{
-					var buttonGUI = _parentObject as ButtonGUI;
-					if (buttonGUI != null)
 					{
-						buttonGUI.OnClick += InvokeEvent;
+						var buttonGUI = _parentObject as ButtonGUI;
+						if (buttonGUI != null)
+						{
+							buttonGUI.OnClick += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.Break:
-				{
-					var limb = _parentObject as Limb;
-					if (limb != null)
 					{
-						limb.OnBreak += InvokeEvent;
+						var limb = _parentObject as Limb;
+						if (limb != null)
+						{
+							limb.OnBreak += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.Spawn:
-				{
-					var enemy = _parentObject as Enemy;
-					if (enemy != null)
 					{
-						enemy.Spawn += InvokeEvent;
+						var enemy = _parentObject as Enemy;
+						if (enemy != null)
+						{
+							enemy.Spawn += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.Attack:
-				{
-					var enemy = _parentObject as Enemy;
-					if (enemy != null)
 					{
-						enemy.Attack += InvokeEvent;
+						var enemy = _parentObject as Enemy;
+						if (enemy != null)
+						{
+							enemy.Attack += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.Walk:
-				{
-					var enemy = _parentObject as Enemy;
-					if (enemy != null)
 					{
-						enemy.Walk += InvokeEvent;
+						var enemy = _parentObject as Enemy;
+						if (enemy != null)
+						{
+							enemy.Walk += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.Death:
-				{
-					var livingObject = _parentObject as LivingObject;
-					if (livingObject != null)
 					{
-						livingObject.Death += InvokeEvent;
+						var livingObject = _parentObject as LivingObject;
+						if (livingObject != null)
+						{
+							livingObject.Death += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.DeathEnd:
-				{
-					var livingObject = _parentObject as LivingObject;
-					if (livingObject != null)
 					{
-						livingObject.DeathEnd += InvokeEvent;
+						var livingObject = _parentObject as LivingObject;
+						if (livingObject != null)
+						{
+							livingObject.DeathEnd += InvokeEvent;
+						}
 					}
-				}
 					break;
 				case Event.Hit:
-				{
-					var projectile = _parentObject as Projectile;
-					if (projectile != null)
 					{
-						projectile.Hit += InvokeEvent;
+						var projectile = _parentObject as Projectile;
+						if (projectile != null)
+						{
+							projectile.Hit += InvokeEvent;
+						}
+						var livingObject = _parentObject as LivingObject;
+						if (livingObject != null)
+						{
+							livingObject.Hit += InvokeEvent;
+						}
 					}
-					var livingObject = _parentObject as LivingObject;
-					if (livingObject != null)
-					{
-						livingObject.Hit += InvokeEvent;
-					}
-				}
 					break;
 				case Event.GameOver:
-				{
-					var player = _parentObject as Player;
-					if (player != null)
 					{
-						player.OnGameOver += InvokeEvent;
+						var player = _parentObject as Player;
+						if (player != null)
+						{
+							player.OnGameOver += InvokeEvent;
+						}
 					}
-				}
+					break;
+				case Event.None:
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		private void UnhookEvents()
+		{
+			switch (_model.Event)
+			{
+				case Event.Interrupt:
+					{
+						var boss = _parentObject as Boss;
+						if (boss != null)
+						{
+							boss.OnInterrupt -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.Click:
+					{
+						var buttonGUI = _parentObject as ButtonGUI;
+						if (buttonGUI != null)
+						{
+							buttonGUI.OnClick -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.Break:
+					{
+						var limb = _parentObject as Limb;
+						if (limb != null)
+						{
+							limb.OnBreak -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.Spawn:
+					{
+						var enemy = _parentObject as Enemy;
+						if (enemy != null)
+						{
+							enemy.Spawn -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.Attack:
+					{
+						var enemy = _parentObject as Enemy;
+						if (enemy != null)
+						{
+							enemy.Attack -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.Walk:
+					{
+						var enemy = _parentObject as Enemy;
+						if (enemy != null)
+						{
+							enemy.Walk -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.Death:
+					{
+						var livingObject = _parentObject as LivingObject;
+						if (livingObject != null)
+						{
+							livingObject.Death -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.DeathEnd:
+					{
+						var livingObject = _parentObject as LivingObject;
+						if (livingObject != null)
+						{
+							livingObject.DeathEnd -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.Hit:
+					{
+						var projectile = _parentObject as Projectile;
+						if (projectile != null)
+						{
+							projectile.Hit -= InvokeEvent;
+						}
+						var livingObject = _parentObject as LivingObject;
+						if (livingObject != null)
+						{
+							livingObject.Hit -= InvokeEvent;
+						}
+					}
+					break;
+				case Event.GameOver:
+					{
+						var player = _parentObject as Player;
+						if (player != null)
+						{
+							player.OnGameOver -= InvokeEvent;
+						}
+					}
 					break;
 				case Event.None:
 					break;
@@ -141,118 +263,6 @@ namespace Scripts.ViewModels.Enemies
 			{
 				_isInvoked = true;
 				base.OnActivate();
-			}
-		}
-
-		protected override void OnDeactivate()
-		{
-			switch (_model.Event)
-			{
-				case Event.Interrupt:
-				{
-					var boss = _parentObject as Boss;
-					if (boss != null)
-					{
-						boss.OnInterrupt -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.Click:
-				{
-					var buttonGUI = _parentObject as ButtonGUI;
-					if (buttonGUI != null)
-					{
-						buttonGUI.OnClick -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.Break:
-				{
-					var limb = _parentObject as Limb;
-					if (limb != null)
-					{
-						limb.OnBreak -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.Spawn:
-				{
-					var enemy = _parentObject as Enemy;
-					if (enemy != null)
-					{
-						enemy.Spawn -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.Attack:
-				{
-					var enemy = _parentObject as Enemy;
-					if (enemy != null)
-					{
-						enemy.Attack -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.Walk:
-				{
-					var enemy = _parentObject as Enemy;
-					if (enemy != null)
-					{
-						enemy.Walk -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.Death:
-				{
-					var livingObject = _parentObject as LivingObject;
-					if (livingObject != null)
-					{
-						livingObject.Death -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.DeathEnd:
-				{
-					var livingObject = _parentObject as LivingObject;
-					if (livingObject != null)
-					{
-						livingObject.DeathEnd -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.Hit:
-				{
-					var projectile = _parentObject as Projectile;
-					if (projectile != null)
-					{
-						projectile.Hit -= InvokeEvent;
-					}
-					var livingObject = _parentObject as LivingObject;
-					if (livingObject != null)
-					{
-						livingObject.Hit -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.GameOver:
-				{
-					var player = _parentObject as Player;
-					if (player != null)
-					{
-						player.OnGameOver -= InvokeEvent;
-					}
-				}
-					break;
-				case Event.None:
-					break;
-				default:
-					throw new ArgumentOutOfRangeException();
-			}
-
-			if (_isInvoked)
-			{
-				_isInvoked = false;
-				base.OnDeactivate();
 			}
 		}
 	}
